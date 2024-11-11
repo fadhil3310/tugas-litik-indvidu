4c60f5d948e473f35c8212317f72c8b7d0191efe
fadhil3310 <virgianofadhil@gmail.com>
2024-11-12 05:13:20 +0700

    perubahan

--- tugas2_Virgiano Fadhil_2409439.py
+++ tugas2_Virgiano Fadhil_2409439.py
@@ -2,14 +2,14 @@
 # NIM: 2409439
 # Kelas: 1B
 
-def login(password):
-    return password == "Latihan"
+def login(username, password):
+    return username == "dudul" and password == "Latihan"
 
 try_count = 0
 while try_count < 3:
     username = input("Masukkan username: ")
     password = input("Masukkan password: ")
-    if (login(password)):
+    if (login(username, password)):
         print("Berhasil login!")
         break
     else:
